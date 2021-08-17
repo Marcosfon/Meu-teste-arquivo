cad4efa6f71b60cf233b26be112e9d037c01b183
Marcosfon <fs37158@gmail.com>
2021-08-16 22:03:38 -0300

    first commit

---
+++ Primeiroteste.java
@@ -0,0 +1,30 @@
+package com.dio;
+
+import com.dio.model.SegundoTeste;
+
+public class Primeiroteste {
+
+	public static void main(String[] args) {
+		
+		SegundoTeste segundoteste = new SegundoTeste();
+		TerceiroTeste terceiroteste = new TerceiroTeste();
+		
+		System.out.println(segundoteste);
+		System.out.println(terceiroteste);
+		
+		/*int a = 2;
+		int b = 3;
+		int c = 6;
+
+			System.out.println("Hello World! " + (a+b*c));*/
+	}
+
+}
+
+	
+	
+class TerceiroTeste {
+	private String nome;
+	private String npag;
+	
+}
